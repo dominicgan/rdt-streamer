1eac02334041dbe6ff21d9b2f4663ff8a324a1bf
Dominic Gan <dominicgangx@gmail.com>
2018-05-16 14:35:36 +0800

    feat(js): split out snoo client to separate file

--- app.js
+++ app.js
@@ -1,21 +1,7 @@
 require('dotenv').config();
 
-const Snoowrap = require('snoowrap');
-const Snoostorm = require('snoostorm');
+const client = require('./snoo.js');
 const prettyjson = require('prettyjson');
-
-// START: Snoo Polling Setup
-// Build Snoowrap and Snoosatorm clients
-const r = new Snoowrap({
-	userAgent: 'reddit-bot-example-node',
-	clientId: process.env.CLIENT_ID,
-	clientSecret: process.env.CLIENT_SECRET,
-	username: process.env.REDDIT_USER,
-	password: process.env.REDDIT_PASS
-});
-
-const client = new Snoostorm(r);
-// END: Snoo Polling Setup
 
 const subreddit = 'singapore';
 
